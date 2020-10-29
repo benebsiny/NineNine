4d8a4e3a5e441a12c1a06b0c6a57a4da99b13ce6
benebsiny <stu995106@hotmail.com.tw>
2020-10-29 09:50:26 +0800

    Login / Signup by 'User' class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("99_home.fxml"));
-        Scene scene = new Scene(root, 1280, 720);
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
