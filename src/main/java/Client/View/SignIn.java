3ed161ce7a47a11dfa878b62e66ffbc7aead2d01
林則穎 <dragon95070@gmail.com>
2020-11-24 16:58:46 +0800

    Add Register fxml and return button

--- src/main/java/Client/View/SignIn.java
+++ src/main/java/Client/View/SignIn.java
@@ -1,4 +1,14 @@
 package Client.View;
 
+import Client.Main;
+import javafx.event.ActionEvent;
+
 public class SignIn {
+
+    public void goBack(ActionEvent actionEvent) {
+        Main.switchScene("Home");
+    }
+    public void register(ActionEvent actionEvent) {
+        Main.switchScene("register");
+    }
 }
