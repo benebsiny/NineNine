84255d101c932f1a01baec73b49e0bc5a924d0b8
林則穎 <dragon95070@gmail.com>
2020-11-24 20:00:09 +0800

    Add Register in shared

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -1,15 +1,65 @@
 package Server;
 
-import java.io.IOException;
+import Shared.PlayCommand;
+import Shared.Register;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(8888);
-            Socket socket = server.accept();
 
+    static class ExecuteClientThread implements Runnable {
+        private Socket client;
+
+        ObjectInputStream in = null;
+        ObjectOutputStream out = null;
+        byte [] buf = new byte[1000];
+
+        ExecuteClientThread(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new ObjectOutputStream(client.getOutputStream());
+
+                in = new ObjectInputStream(client.getInputStream());
+
+                Object temp = in.readObject();
+                if(temp instanceof PlayCommand) {
+
+                }
+                else if(temp instanceof Register){
+
+                }
+
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+    }
+    public static void main(String[] args) {
+
+
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(100);
+            ServerSocket server = new ServerSocket(8888);
+            for(int i = 0; i < 5; i ++) {
+                Socket socket = server.accept();
+                executorService.execute(new ExecuteClientThread(socket));
+            }
+            executorService.shutdown();
+            server.close();
             //TODO handle connection
         } catch (IOException e) {
             e.printStackTrace();
