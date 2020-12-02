2373b73250d7aab09964ad9807595fb3dc64c6e1
benebsiny <stu995106@hotmail.com.tw>
2020-12-02 16:36:38 +0800

    Use Arrays.copyOf instead System.arraycopy

--- src/main/java/Client/Status/PlayerStatus.java
+++ src/main/java/Client/Status/PlayerStatus.java
@@ -1,15 +1,15 @@
 package Client.Status;
 
+import java.util.Arrays;
+
 public class PlayerStatus {
     private static String[] players;
 
     public static String[] getPlayers() {
-        String[] clonePlayers = new String[players.length];
-        System.arraycopy(PlayerStatus.players, 0, clonePlayers, 0, players.length);
-        return clonePlayers;
+        return Arrays.copyOf(PlayerStatus.players, PlayerStatus.players.length);
     }
 
     public static void setPlayers(String[] players) {
-        System.arraycopy(players, 0, PlayerStatus.players, 0, players.length);
+        PlayerStatus.players = Arrays.copyOf(players, players.length);
     }
 }
